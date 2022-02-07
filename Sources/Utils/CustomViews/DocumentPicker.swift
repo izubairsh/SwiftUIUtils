ccf6d7a16a4a387b10f216621d988c994f72b2a6
Zubair Sheikh <izubairsh@gmail.com>
2022-02-07 19:25:27 +0500

    changes in DocumentPicker

--- Sources/Utils/CustomViews/DocumentPicker.swift
+++ Sources/Utils/CustomViews/DocumentPicker.swift
@@ -9,38 +9,67 @@ import Foundation
 import SwiftUI
 import MobileCoreServices
 
+#if os(iOS) || targetEnvironment(macCatalyst)
+
+import SwiftUI
+
+/// A SwiftUI port of `UIDocumentPickerViewController`.
 public struct DocumentPicker: UIViewControllerRepresentable {
+    public typealias UIViewControllerType = UIDocumentPickerViewController
     
-    public var documentTypes: [String]
-    public var callback: (URL) -> ()
+    @Environment(\.presentationMode) var presentationMode: Binding<PresentationMode>
     
-    public init(documentTypes: [String], callback: @escaping (URL) -> ()) {
-        self.callback = callback
-        self.documentTypes = documentTypes
-    }
-
-    public func makeCoordinator() -> Coordinator { Coordinator(self) }
-
-    public func updateUIViewController(_ uiViewController: UIDocumentPickerViewController, context: UIViewControllerRepresentableContext<DocumentPicker>) {
-    }
-
-    public func makeUIViewController(context: Context) -> UIDocumentPickerViewController {
-        let controller = UIDocumentPickerViewController(documentTypes: documentTypes, in: .import)
-        controller.allowsMultipleSelection = false
+    var url: Binding<URL?>?
+    var onCancel: (() -> Void)?
+    
+    public func makeUIViewController(context: Context) -> UIViewControllerType {
+        let controller = UIDocumentPickerViewController(documentTypes: [String(kUTTypePDF)], in: .open)
         controller.delegate = context.coordinator
         return controller
     }
-
-    public class Coordinator: NSObject, UIDocumentPickerDelegate {
-        public var parent: DocumentPicker
-        public init(_ pickerController: DocumentPicker) {
-            self.parent = pickerController
+    
+    public func updateUIViewController(_ uiViewController: UIViewControllerType, context: Context) {
+        context.coordinator.base = self
+    }
+    
+    public class Coordinator: NSObject, UIDocumentPickerDelegate, UINavigationControllerDelegate {
+        var base: DocumentPicker
+        
+        init(base: DocumentPicker) {
+            self.base = base
         }
+        
         public func documentPicker(_ controller: UIDocumentPickerViewController, didPickDocumentsAt urls: [URL]) {
-            parent.callback(urls[0])
+            base.url?.wrappedValue = urls[0]
+            base.presentationMode.wrappedValue.dismiss()
         }
+        
         public func documentPickerWasCancelled(_ controller: UIDocumentPickerViewController) {
+            if let onCancel = base.onCancel {
+                onCancel()
+            } else {
+                base.presentationMode.wrappedValue.dismiss()
+            }
         }
+    }
+    
+    public func makeCoordinator() -> Coordinator {
+        .init(base: self)
     }
 }
 
+// MARK: - API -
+
+extension DocumentPicker {
+    
+    public init(
+        url: Binding<URL?>,
+        onCancel: (() -> Void)? = nil
+    ) {
+        self.url = url
+        self.onCancel = onCancel
+    }
+    
+}
+
+#endif
