0154f9b1f6177d71fb9f6a04667e6359c6e4bc35
Zubair Sheikh <izubairsh@gmail.com>
2022-02-08 17:46:42 +0500

    changes

--- Sources/Utils/CustomViews/DocumentPicker.swift
+++ Sources/Utils/CustomViews/DocumentPicker.swift
@@ -39,9 +39,44 @@ public struct DocumentPicker: UIViewControllerRepresentable {
             self.base = base
         }
         
-        public func documentPicker(_ controller: UIDocumentPickerViewController, didPickDocumentsAt urls: [URL]) {
-            base.url?.wrappedValue = urls[0]
-            base.presentationMode.wrappedValue.dismiss()
+        public func documentPicker(_ controller: UIDocumentPickerViewController, didPickDocumentAt url: URL) {
+            // Start accessing a security-scoped resource.
+                guard url.startAccessingSecurityScopedResource() else {
+                    // Handle the failure here.
+                    return
+                }
+
+                // Make sure you release the security-scoped resource when you finish.
+                defer { url.stopAccessingSecurityScopedResource() }
+
+                // Use file coordination for reading and writing any of the URL’s content.
+                var error: NSError? = nil
+                NSFileCoordinator().coordinate(readingItemAt: url, error: &error) { (url) in
+                        
+                    let keys : [URLResourceKey] = [.nameKey, .isDirectoryKey]
+                        
+                    // Get an enumerator for the directory's content.
+                    guard let fileList =
+                        FileManager.default.enumerator(at: url, includingPropertiesForKeys: keys) else {
+                        Swift.debugPrint("*** Unable to access the contents of \(url.path) ***\n")
+                        return
+                    }
+                        
+                    for case let file as URL in fileList {
+                        // Start accessing the content's security-scoped URL.
+                        guard url.startAccessingSecurityScopedResource() else {
+                            // Handle the failure here.
+                            continue
+                        }
+
+                        // Do something with the file here.
+                        Swift.debugPrint("chosen file: \(file.lastPathComponent)")
+                        base.url?.wrappedValue = file
+                        // Make sure you release the security-scoped resource when you finish.
+                        url.stopAccessingSecurityScopedResource()
+                        base.presentationMode.wrappedValue.dismiss()
+                    }
+                }
         }
         
         public func documentPickerWasCancelled(_ controller: UIDocumentPickerViewController) {
