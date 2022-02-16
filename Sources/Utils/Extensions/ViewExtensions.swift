fbf2443b6577ff10ab6470c31aa9e80305d12c23
Zubair Sheikh <izubairsh@gmail.com>
2022-02-16 18:25:50 +0500

    added extensions

--- Sources/Utils/Extensions/ViewExtensions.swift
+++ Sources/Utils/Extensions/ViewExtensions.swift
@@ -37,3 +37,29 @@ extension View {
         self.modifier(SideMenu(isShowing: isShowing, menuContent: menuContent))
     }
 }
+
+
+// Extenting View to get SCreen Rect...
+extension View{
+    public func getRect()->CGRect{
+        return UIScreen.main.bounds
+    }
+    
+    public func safeArea()->UIEdgeInsets{
+        let null = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+        
+        guard let screen = UIApplication.shared.connectedScenes.first as? UIWindowScene else{
+            return null
+        }
+        
+        guard let safeArea = screen.windows.first?.safeAreaInsets else{
+            return null
+        }
+        
+        return safeArea
+    }
+    
+    public func getScreenBounds()->CGRect{
+        return UIScreen.main.bounds
+    }
+}
