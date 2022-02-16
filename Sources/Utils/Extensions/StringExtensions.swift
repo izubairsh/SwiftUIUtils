70270c9cd12767e482bf891a8aef0eef54656510
Zubair Sheikh <izubairsh@gmail.com>
2022-02-16 18:35:27 +0500

    extension added to convert string to hex

--- Sources/Utils/Extensions/StringExtensions.swift
+++ Sources/Utils/Extensions/StringExtensions.swift
@@ -57,4 +57,28 @@ extension String {
         return nil
     }
     
+    func hexToColor () -> Color {
+            var cString:String = self.trimmingCharacters(in: .whitespacesAndNewlines).uppercased()
+
+            if (cString.hasPrefix("#")) {
+                cString.remove(at: cString.startIndex)
+            }
+
+            if ((cString.count) != 6) {
+                return .gray
+            }
+
+            var rgbValue:UInt64 = 0
+            Scanner(string: cString).scanHexInt64(&rgbValue)
+
+            let color = UIColor(
+                red: CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0,
+                green: CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0,
+                blue: CGFloat(rgbValue & 0x0000FF) / 255.0,
+                alpha: CGFloat(1.0)
+            )
+            
+            return Color(color)
+        }
+    
 }
