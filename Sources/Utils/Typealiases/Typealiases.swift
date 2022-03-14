8ae9248faa4a7db5d56cd0eb0bd82c260aa6b44a
Zubair Sheikh <izubairsh@gmail.com>
2022-03-14 18:06:45 +0500

    changes

--- Sources/Utils/Typealiases/Typealiases.swift
+++ Sources/Utils/Typealiases/Typealiases.swift
@@ -6,9 +6,3 @@
 //
 
 import Foundation
-
-public typealias EmptyCompletion = () -> Void
-public typealias CompletionObject<T> = (_ response: T) -> Void
-public typealias CompletionOptionalObject<T> = (_ response: T?) -> Void
-public typealias CompletionResponse = (_ response: Result<Void, Error>) -> Void
-
